48f2abd8d0d9ff47d3d63f6577309d101cb26506
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-04-24 18:23:25 +0200

    added project-component

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { ProjectComponent } from './project/project.component';
 
 @Component({
   selector: 'app-portfolio',
-  imports: [],
+  imports: [ProjectComponent],
   templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.scss'
+  styleUrl: './portfolio.component.scss',
 })
-export class PortfolioComponent {
-
-}
+export class PortfolioComponent {}
