c3ae3b24d56baa371dffa8defbf7c4dcb4efcbd2
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-04-29 17:44:11 +0200

    added animation to reference-cards when switch in carousel

--- src/app/references/references.component.ts
+++ src/app/references/references.component.ts
@@ -14,14 +14,36 @@ export class ReferencesComponent {
   references = REFERENCES;
   currentSlideIndex = 0;
 
-  prevSlide(): void {
-    this.currentSlideIndex =
-      (this.currentSlideIndex - 1 + this.references.length) %
-      this.references.length;
+  get displayedReferences() {
+    const len = this.references.length;
+    const left = (this.currentSlideIndex - 1 + len) % len;
+    const center = this.currentSlideIndex;
+    const right = (this.currentSlideIndex + 1) % len;
+    return [
+      { ...this.references[left], position: 'left' },
+      { ...this.references[center], position: 'center' },
+      { ...this.references[right], position: 'right' },
+    ];
   }
 
+  slideDirection: 'left' | 'right' | null = null;
+
   nextSlide(): void {
-    this.currentSlideIndex =
-      (this.currentSlideIndex + 1) % this.references.length;
+    this.slideDirection = 'left';
+    setTimeout(() => {
+      this.currentSlideIndex =
+        (this.currentSlideIndex + 1) % this.references.length;
+      this.slideDirection = null;
+    }, 500); // Dauer der CSS-Animation
+  }
+
+  prevSlide(): void {
+    this.slideDirection = 'right';
+    setTimeout(() => {
+      this.currentSlideIndex =
+        (this.currentSlideIndex - 1 + this.references.length) %
+        this.references.length;
+      this.slideDirection = null;
+    }, 500);
   }
 }
