fa2a45fd24ec90f6f4c1ed63aa1fd25442de1f45
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-04-30 17:19:58 +0200

    removed not needed imports

--- src/app/references/references.component.ts
+++ src/app/references/references.component.ts
@@ -2,18 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReferenceCardComponent } from './reference-card/reference-card.component';
 import { REFERENCES } from './reference.data';
-import { trigger, transition, animate } from '@angular/animations';
+
 @Component({
   selector: 'app-references',
   standalone: true,
   imports: [CommonModule, ReferenceCardComponent],
   templateUrl: './references.component.html',
   styleUrl: './references.component.scss',
-  animations: [
-    trigger('carouselAnimation', [
-      transition('* => *', [animate('300ms ease-in-out')]),
-    ]),
-  ],
 })
 export class ReferencesComponent {
   references = REFERENCES;
