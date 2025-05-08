9795bc4c133868eaba32d67063866db3f5caa0f4
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-05-08 13:17:10 +0200

    added validation to contact-form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -13,7 +13,34 @@ export class ContactComponent {
     email: '',
     message: '',
   };
-  onSubmit() {
-    console.log(this.contactData);
+
+  // mailTest = true;
+
+  // post = {
+  //   endPoint: 'https://deineDomain.de/sendMail.php',
+  //   body: (payload: any) => JSON.stringify(payload),
+  //   options: {
+  //     headers: {
+  //       'Content-Type': 'text/plain',
+  //       responseType: 'text',
+  //     },
+  //   },
+  // };
+
+  onSubmit(ngForm: NgForm) {
+    //   if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+    //     this.http.post(this.post.endPoint, this.post.body(this.contactData))
+    //       .subscribe({
+    //         next: (response) => {
+    //           ngForm.resetForm();
+    //         },
+    //         error: (error) => {
+    //           console.error(error);
+    //         },
+    //         complete: () => console.info('send post complete'),
+    //       });
+    //   } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+    //     ngForm.resetForm();
+    //   }
   }
 }
