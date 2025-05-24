1281835d970725a4c27d6fa690fe4d3fdd6e52ed
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-05-25 00:44:56 +0200

    added favicon-service to change favicon based on users choice of light or dark theme

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,13 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { FaviconService } from './shared/services/favicon.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), provideHttpClient()],
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    provideHttpClient(),
+    FaviconService,
+  ],
 };
--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,25 @@ import { AppComponent } from './app/app.component';
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
 );
+
+// Initial favicon setup based on system theme
+const favicon = document.getElementById('favicon') as HTMLLinkElement | null;
+if (
+  window.matchMedia &&
+  window.matchMedia('(prefers-color-scheme: dark)').matches
+) {
+  if (favicon) {
+    favicon.href = 'favicon_dark_theme.png';
+  }
+}
+
+// Watch for theme changes
+window
+  .matchMedia('(prefers-color-scheme: dark)')
+  .addEventListener('change', (e) => {
+    if (favicon) {
+      favicon.href = e.matches
+        ? 'favicon_dark_theme.png'
+        : 'favicon_light_theme.png';
+    }
+  });
