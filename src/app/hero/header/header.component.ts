f7192dac6aa74b842902c411ff431fb7dd0e3212
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-05-28 17:54:23 +0200

    added burger-menu to header-component

--- src/app/hero/header/header.component.ts
+++ src/app/hero/header/header.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TranslationService } from '../../shared/services/translation.service';
 import { CommonModule } from '@angular/common';
 import { take } from 'rxjs/operators';
-import { MobileMenuComponent } from './mobile-menu/mobile-menu.component'; // Neuer Import für take
+import { MobileMenuComponent } from './mobile-menu/mobile-menu.component';
 
 @Component({
   selector: 'app-header',
@@ -11,6 +11,8 @@ import { MobileMenuComponent } from './mobile-menu/mobile-menu.component'; // Ne
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  mobileMenuOpen = false;
+
   constructor(public translationService: TranslationService) {}
 
   ngOnInit() {
@@ -27,5 +29,9 @@ export class HeaderComponent implements OnInit {
     this.translationService.isGerman$.pipe(take(1)).subscribe((isGerman) => {
       this.translationService.setLanguage(!isGerman);
     });
+  }
+
+  toggleMobileMenu() {
+    this.mobileMenuOpen = !this.mobileMenuOpen;
   }
 }
