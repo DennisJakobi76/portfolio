4ba67184649995df1ab8cb6cfc12f924715abdc0
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-06-16 17:46:47 +0200

    added functionality to close mobile menu by clicking outside of the mobile-menu-component

--- src/app/hero/header/header.component.ts
+++ src/app/hero/header/header.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { TranslationService } from '../../shared/services/translation.service';
+import { Component, ElementRef, HostListener, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { take } from 'rxjs/operators';
 import { MobileMenuComponent } from './mobile-menu/mobile-menu.component';
+import { TranslationService } from '../../shared/services/translation.service';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -10,19 +10,27 @@ import { MobileMenuComponent } from './mobile-menu/mobile-menu.component';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   mobileMenuOpen = false;
 
-  constructor(public translationService: TranslationService) {}
+  @ViewChild('burgerMenu', { read: ElementRef }) burgerMenuRef!: ElementRef;
+  @ViewChild(MobileMenuComponent, { read: ElementRef }) mobileMenuRef!: ElementRef;
+
+  private documentClickListener: any;
+
+  constructor(public translationService: TranslationService, private elementRef: ElementRef) {}
 
   ngOnInit() {
-    // Initialer Zustand
     this.translationService.isGerman$.subscribe((isGerman) => {
       const languageSwitch = document.getElementById('language-switch');
       if (languageSwitch) {
         languageSwitch.classList.toggle('german', isGerman);
       }
     });
+  }
+
+  ngOnDestroy() {
+    this.removeDocumentClickListener();
   }
 
   toggleLanguage() {
@@ -33,5 +41,35 @@ export class HeaderComponent implements OnInit {
 
   toggleMobileMenu() {
     this.mobileMenuOpen = !this.mobileMenuOpen;
+    if (this.mobileMenuOpen) {
+      this.addDocumentClickListener();
+    } else {
+      this.removeDocumentClickListener();
+    }
+  }
+
+  addDocumentClickListener() {
+    this.documentClickListener = (event: MouseEvent) => {
+      // Finde das menu-container-Element innerhalb von app-mobile-menu
+      const menuEl = this.elementRef.nativeElement.querySelector('app-mobile-menu .menu-container');
+      const burgerEl = this.elementRef.nativeElement.querySelector('.burger-menu-wrapper');
+      if (
+        menuEl &&
+        !menuEl.contains(event.target as Node) &&
+        burgerEl &&
+        !burgerEl.contains(event.target as Node)
+      ) {
+        this.mobileMenuOpen = false;
+        this.removeDocumentClickListener();
+      }
+    };
+    document.addEventListener('mousedown', this.documentClickListener, true);
+  }
+
+  removeDocumentClickListener() {
+    if (this.documentClickListener) {
+      document.removeEventListener('mousedown', this.documentClickListener, true);
+      this.documentClickListener = null;
+    }
   }
 }
