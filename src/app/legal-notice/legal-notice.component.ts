696528fec00af419f06cb46870cc0443c5ff73dc
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-07-10 15:17:26 +0200

    changed legal-components completely and added header and footrt

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TranslationService } from '../shared/services/translation.service';
-import { RouterLink } from '@angular/router';
+import { FooterComponent } from '../footer/footer.component';
+import { HeaderComponent } from '../hero/header/header.component';
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [RouterLink],
+  imports: [FooterComponent, HeaderComponent],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss',
 })
